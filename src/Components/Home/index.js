199fb279c16c937263b14dc238263239cd078483
rovsencode <rovshanakh@code.edu.az>
2023-03-24 21:50:05 +0400

    add material ui

---
+++ src/Components/Home/index.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+
+export default function Home() {
+  return (
+    <div>
+      <Button variant="contained">Hello World</Button>
+      </div>
+     
+    
+      
+  );
+}
